1f0d0498374cea8339fb088aa0df6730eaeb7fc9
ShakthiPerera <shakthiperera231@gmail.com>
2022-04-09 07:37:31 +0530

    Added the Alpha Beta Pruning

--- TicTac_Class.py
+++ TicTac_Class.py
@@ -35,11 +35,12 @@ class TicTac:
       elif self.board[i] == ['O', 'O', 'O']:
         return 'O'
       # Vertical
-      elif self.board[0][i] == self.board[1][i] and self.board[1][i] == self.board[2][i] and self.board[2][i] != '.':
+    for i in range(3):
+      if self.board[0][i] == self.board[1][i] and self.board[1][i] == self.board[2][i] and self.board[2][i] != '.':
         return self.board[0][i]
     if self.board[0][0] == self.board[1][1] and  self.board[1][1] == self.board[2][2] and self.board[2][2] != '.':
       return self.board[0][0]
-    elif self.board[0][2] == self.board[1][1] and  self.board[1][1] == self.board[2][0] and self.board[1][1] != '.':
+    if self.board[0][2] == self.board[1][1] and  self.board[1][1] == self.board[2][0] and self.board[1][1] != '.':
       return self.board[1][1]
 
     dot_count = 0
@@ -52,7 +53,7 @@ class TicTac:
     return None
 
   #max player is 'O'
-  def max(self):
+  def max(self,alpha, beta):
     max_score = -2
     px = py = 0
     result = self.is_end()
@@ -62,41 +63,51 @@ class TicTac:
         return 1, 0, 0
     elif result == 'TIE':
         return 0, 0, 0
-    else:
-      for i in range(3):
-        for j in range(3):
-          if self.is_valid('O', i, j):
-              self.board[i][j] = 'O'
-              m, px, py = self.min()
-              if m > max_score:
-                  max_score = m
-                  px = i
-                  py = j
-              self.board[i][j] = '.'
+    for i in range(3):
+      for j in range(3):
+        if self.is_valid('O', i, j):
+            self.board[i][j] = 'O'
+            m, min_i, min_j = self.min(alpha, beta)
+            if m > max_score:
+                max_score = m
+                px = i
+                py = j
+            self.board[i][j] = '.'
+
+            if max_score >= beta:
+              return max_score, px, py
+
+            if max_score > alpha:
+              alpha = max_score
     return max_score, px, py
 
-  def min(self):
+  def min(self, alpha, beta):
     min_score = 2
-    px = py = None
+    qx = qy = None
     result = self.is_end()
     if result == 'X':
-        return 1, 0, 0
-    elif result == 'O':
         return -1, 0, 0
+    elif result == 'O':
+        return 1, 0, 0
     elif result == 'TIE':
         return 0, 0, 0
-    else:
-      for i in range(3):
-        for j in range(3):
-          if self.is_valid('X', i, j):
-              self.board[i][j] = 'X'
-              m, px, py = self.max()
-              if m < min_score:
-                  min_score = m
-                  px = i
-                  py = j
-              self.board[i][j] = '.'
-    return min_score, px, py
+    for i in range(3):
+      for j in range(3):
+        if self.is_valid('X', i, j):
+            self.board[i][j] = 'X'
+            m, maxx, maxy = self.max(alpha, beta)
+            if m < min_score:
+                min_score = m
+                qx = i
+                qy = j
+            self.board[i][j] = '.'
+
+            if min_score <= alpha:
+              return min_score, qx, qy
+
+            if min_score < beta:
+              beta = min_score
+    return min_score, qx, qy
 
   def play(self):
     playerMark = 'X'
@@ -114,16 +125,15 @@ class TicTac:
         print(f"This is the {playerMark} Turn !!!")
         print()
 
-        print()
+
         if playerMark == 'X':
           row = int(input("Enter the Row Number:"))
           col = int(input("Enter the Column Number:"))
-
           self.make_move(playerMark, row, col)
           print()
           playerMark = 'O'
         elif playerMark == 'O':
-          max_score, row, col = self.max()
+          max_score, row, col = self.max(-5, 5)
           self.make_move(playerMark, row, col)
           print()
           playerMark = 'X'
